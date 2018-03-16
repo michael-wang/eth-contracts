e418785eb7c4b004295c6b414b466b99cc9fc6be
viwave-michael <michael.wang@viwave>
2018-03-16 12:47:48 +0800

    Add selling function.

--- my_advanced_token.s
+++ my_advanced_token.s
@@ -87,10 +87,27 @@ contract MyAdvancedToken is owned {
 
   function buy() payable returns (uint amount) {
     amount = msg.value / buyPrice;
-    require (balanceOf[this] >= amount);
+    require (balanceOf[this] >= amount &&
+      balanceOf[msg.sender] + amount >= balanceOf[msg.sender]);
+
     balanceOf[msg.sender] += amount;
     balanceOf[this] -= amount;
+
     Transfer(this, msg.sender, amount);
     return amount;
+  }
+
+  function sell(uint amount) returns (uint revenue) {
+    require (balanceOf[msg.sender] >= amount &&
+      balanceOf[this] + amount >= balanceOf[this]);
+
+    balanceOf[this] += amount;
+    balanceOf[msg.sender] -= amount;
+
+    revenue = amount * sellPrice;
+    msg.sender.transfer(revenue);
+
+    Transfer(msg.sender, this, amount);
+    return revenue;
   }
 }
